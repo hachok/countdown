7d62b0a021ee82fd636893fb284c5d97594916ce
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-08-08 19:21:11 +0200

    init app

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import { Page, Layout, TextStyle } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Layout>
+      <p>Sample app using React and Next.js</p>
+      <TextStyle variation="positive">
+        Sample app using React and Next.js
+      </TextStyle>
+    </Layout>
+  </Page>
+);
+
+export default Index;
