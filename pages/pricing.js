36a00b7301529426489968d54aace3929aef1b46
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-08-09 22:14:32 +0200

    add list of items

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,12 +1,41 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Card, Layout, Page, ResourceList, Avatar, TextStyle } from "@shopify/polaris";
 
 const Pricing = () => (
   <Page>
-    <Heading>You created a new page called Pricing</Heading>
-    <p>You can visit this page by appending "/Pricing" to your URL</p>
-    <p>You can edit this page in "/pages/Pricing</p>
-    <p>For information on Next.js dynamic routing <a href="https://nextjs.org/learn/basics/navigate-between-pages" target="_blank">check out their documentation</a></p>
-    <p>For information about navigation within the admin frame, <a href="https://help.shopify.com/en/api/embedded-apps/app-extensions/navigation/create-navigation-link" target="_blank">see the Shopify documentation.</a></p>
+    <Layout>
+      <Card>
+        <ResourceList
+          showHeader
+          items={[
+            {
+              id: 341,
+              url: 'customers/341',
+              name: 'Mae Jemison',
+              location: 'Decatur, USA',
+            },
+            {
+              id: 256,
+              url: 'customers/256',
+              name: 'Ellen Ochoa',
+              location: 'Los Angeles, USA',
+            },
+          ]}
+          renderItem={(item) => {
+            const {id, url, name, location} = item;
+            const media = <Avatar customer size="medium" name={name} />;
+
+            return (
+              <ResourceList.Item id={id} url={url} media={media}>
+                <h3>
+                  <TextStyle variation="strong">{name}</TextStyle>
+                </h3>
+                <div>{location}</div>
+              </ResourceList.Item>
+            );
+          }}
+        />
+      </Card>
+    </Layout>
   </Page>
-)
+);
 export default Pricing;
