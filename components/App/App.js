e0b6201ea24064741bb0f1579eda77b77d0f2bb3
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-08-19 23:52:16 +0200

    remove container

--- components/App/App.js
+++ components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Heading, CalloutCard } from "@shopify/polaris";
-import { Container } from "./App.styled";
 
 class App extends Component {
   render() {
